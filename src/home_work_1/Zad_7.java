e9527be2a63e97aa018e2ea09f1a1a06fa4aab71
Dzmitry Harashchenia <qwester345@ya.ru>
2021-12-06 20:06:56 +0300

    еще раз поправил

--- src/home_work_1/Zad_7.java
+++ src/home_work_1/Zad_7.java
@@ -12,7 +12,7 @@ public class Zad_7 {
     }
     public static String toBinaryString(byte number){
         byte[] binar = new byte[8];
-        String toBinary = "0";
+        String toBinary = "";
         if (number >= 0 ) {
             for (byte i = 0; i < 8; i++) {
                 binar[i] = (byte) (number % 2);
@@ -28,7 +28,7 @@ public class Zad_7 {
 
 
         }
-        toBinary = ""+ binar[7] +  binar[6]+ binar[5]+binar[4]+binar[3]+ binar[2]+binar[1]+binar[0];
+        toBinary = toBinary + binar[7] +  binar[6]+ binar[5]+binar[4]+binar[3]+ binar[2]+binar[1]+binar[0];
         return toBinary;
     }
 }
