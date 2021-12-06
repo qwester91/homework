fa717eac5b6a6c1979f320a143f1fdee74698080
Dzmitry Harashchenia <qwester345@ya.ru>
2021-12-06 20:06:30 +0300

    не дочитал дз, пришлось доделывать

---
+++ src/home_work_1/Zad_3.java
@@ -0,0 +1,24 @@
+package home_work_1;
+
+public class Zad_3 {
+    public static void main(String[] args) {
+        double a = 2.2;
+        double b = 3.1;
+        double c = 3.6;
+        System.out.println(Math.acos(a));
+        System.out.println(Math.asin(a));
+        System.out.println(Math.cos(a));
+        System.out.println(Math.sin(a));
+        System.out.println(Math.abs(a));
+        System.out.println(Math.PI + a);
+        System.out.println(Math.pow(a, b));
+        System.out.println(Math.sqrt(a));
+        System.out.println(Math.cbrt(a));
+        System.out.println(Math.random());
+        System.out.println(Math.round(c));
+        System.out.println(Math.floor(c));
+        System.out.println(Math.ceil(c));
+
+
+    }
+}
